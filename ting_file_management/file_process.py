58127002892b783ee0ab3ec39f48d4065320d4c2
raffael-marinho <silvamarinho10@gmail.com>
2022-10-20 23:59:44 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+import sys
+
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+    if data not in instance._file:
+        instance.enqueue(data)
+    return sys.stdout.write(str(data))
 
 
 def remove(instance):
