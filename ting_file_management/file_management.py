a4ad3f0be8552f1a16aaaeb91d1bd928ea9ac502
raffael-marinho <silvamarinho10@gmail.com>
2022-10-20 23:19:45 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    txt_imported = []
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            for text in file:
+                txt_imported.append(text.strip())
+        return txt_imported
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
