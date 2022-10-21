e43866f80b5e1f6c8df810a2838e2e687ea78821
raffael-marinho <silvamarinho10@gmail.com>
2022-10-21 00:23:31 -0300

    req 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result_list = list()
+    for index in range(instance.__len__()):
+        current = instance.search(index)
+        ocorrencias = list()
+        lines = current["linhas_do_arquivo"]
+
+        for index, line in enumerate(lines):
+            if (word.lower() in line.lower()):
+                ocorrencias.append({"linha": index + 1})
+
+        if (len(ocorrencias) > 0):
+            result = {
+                "palavra": word,
+                "arquivo": current["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            }
+            result_list.append(result)
+    return result_list
 
 
 def search_by_word(word, instance):
